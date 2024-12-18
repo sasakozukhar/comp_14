24b272823f44fe99e5748f9735aac5c601127dd6
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-18 09:01:40 +0200

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            for (int x = 10; x >= 1; x--)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        //static void Func()
+        //{
+        //    for (int x = 1; x <= 10; x++)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //}
+        //static void Func()
+        //{
+        //    int x = 1;
+        //    do
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+
+        //    }
+        //    while (x <= 10);
+        //}
+        //static void Func()
+        //{
+        //    int x = 1;
+        //    while (x<=10)
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;//x+=1; x++
+
+        //    }
+        //}
+        static void Main(string[] args)
+        {
+           Func();
+            Console.Read();
+            
+
+        }
+    }
+}
