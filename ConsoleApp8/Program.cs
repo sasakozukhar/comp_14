a85dd40c282ca10855cc8ebe0f80632229867335
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-18 09:18:14 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Func(int n)
+        {
+            for (int x=1; x<=n; x++)
+            {
+                
+                Console.WriteLine(x);
+            }
+
+        }
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int .Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            
+            
+            int n = Input("n=");
+            Func(n);
+            Console.Read();
+        }
+    }
+}
