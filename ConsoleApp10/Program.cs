6057fd2e2185facfb9a415c54b5c80b00d8bafd2
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-23 17:22:03 +0200

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    /// <summary>
+    ///  С использованием цветового оформления консоли:
+    ///а) вывести на экран горизонтальную строку из 18 символов;
+    ///б) вывести на экран вертикальную строку из 25 символов.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task_A
+
+            //Console.ForegroundColor = ConsoleColor.Blue; 
+            //Console.WriteLine(new string('7', 18)); 
+            //Console.ResetColor(); 
+            //Console.ReadKey();
+
+            //task_B;
+           
+            //Console.ForegroundColor = ConsoleColor.Blue; 
+            //for (int x = 0; x < 25; x++) 
+            //{
+            //    Console.WriteLine("7");
+            //}
+            //Console.ResetColor();
+            //Console.ReadKey();
+        }
+    }
+}
