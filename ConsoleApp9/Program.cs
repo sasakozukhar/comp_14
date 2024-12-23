f8d9d5af6711009f570fc485388e8e74658f3464
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-23 17:10:19 +0200

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            for (int i = 0; i<= 4; i++)
+           {
+                Console.WriteLine("hello");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+    }
+}
